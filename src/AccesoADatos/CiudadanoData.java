330a69bbca9d014d87f62251bcc96870080982c6
Federrrico <federico.acenjo@gmail.com>
2023-10-02 20:57:19 -0300

    CiudadanoData
    Se agregaron metodos:
    agregarCiudadano
    listarCiudadanos

--- src/AccesoADatos/CiudadanoData.java
+++ src/AccesoADatos/CiudadanoData.java
@@ -5,10 +5,76 @@
  */
 package AccesoADatos;
 
+import Entidades.Ciudadano;
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author federico.acenjo
  */
 public class CiudadanoData {
+
+    private Connection con = null;
+
+    public CiudadanoData() {
+        con = Conexion.getConexion();
+    }
+
+    public void guardarCiudadano(Ciudadano ciudadano) {
+
+        String sql = "INSERT INTO ciudadano (dni, nombre_completo, correo_electronico, telefono, patologia, ambito_laboral) VALUES (?, ?, ?, ?, ?, ?)";
+
+        try {
+            PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            ps.setInt(1, ciudadano.getDni());
+            ps.setString(2, ciudadano.getNombre_completo());
+            ps.setString(3, ciudadano.getCorreo_electronico());
+            ps.setInt(4, ciudadano.getTelefono());
+            ps.setString(5, ciudadano.getPatologia());
+            ps.setString(6, ciudadano.getAmbito_laboral());
+            ps.executeUpdate();
+            ResultSet rs = ps.getGeneratedKeys();
+            if (rs.next()) {
+                ciudadano.setDni(rs.getInt(1));
+                JOptionPane.showMessageDialog(null, "Ciudadano añadido con exito.");
+            }
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Ciudadano existente");
+        }
+    }
+    
+    
+    public List<Ciudadano> listarCiudadanos() {
+        List<Ciudadano> ciudadanos = new ArrayList<>();
+        
+        try {
+            String sql = "SELECT * FROM ciudadano";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ResultSet rs = ps.executeQuery();
+            while(rs.next()){
+                Ciudadano ciudadano = new Ciudadano();
+                ciudadano.setDni(rs.getInt("dni"));
+                ciudadano.setNombre_completo(rs.getString("nombre_completo"));
+                ciudadano.setCorreo_electronico(rs.getString("correo_electronico"));
+                ciudadano.setTelefono(rs.getInt("telefono"));
+                ciudadano.setPatologia(rs.getString("patologia"));
+                ciudadano.setAmbito_laboral(rs.getString("ambito_laboral"));
+                ciudadanos.add(ciudadano);
+            } 
+            ps.close();
+        } catch (SQLException ex) {
+            JOptionPane.showMessageDialog(null, "Error al acceder a la tabla Ciudadano");
+        }
+        return ciudadanos;
+    }
     
 }
